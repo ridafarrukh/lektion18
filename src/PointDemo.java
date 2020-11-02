0b0ce82bb9aeb508747c8588e4f38e5b4a178c72
ridafarrukh <ridafarrukh92@gmail.com>
2020-11-03 00:18:49 +0100

    skapa 5 punkter

--- src/PointDemo.java
+++ src/PointDemo.java
@@ -9,5 +9,41 @@
 public class PointDemo {
     public static void main(String[] args) {
 
+        //skapa en punkt
+        Point p1 = new Point();
+        System.out.println("(" + p1.x + "," + p1.y + ")");
+
+       // skapa en array som innehåller 5 referensvariabler
+        Point[] arr = new Point[5];
+
+        //vi har fått en array med 5 referansvariabler
+        for (Point p : arr)
+            System.out.println(p);
+
+        // skapa 5 punkter
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = new Point();
+
+        // skriv ut referenserna
+        for (Point p : arr)
+            System.out.println(p);
+
+        // skriv ur punkterna med for-loop
+        for (Point p : arr)
+            System.out.println("(" + p1.x + "," + p1.y + ")");
+
+        //skriv ut första punkt i arrayen
+        arr[0].x = 3;
+        arr[0].y = 5;
+        System.out.println("(" + arr[0].x + "," + arr[0].y + ")");
+
+
+
+
+
+
+
+
+
     }
 }
